d4a77c6f0122c2b1d92af11323fde72ade84efd4
Marc <81183798+marchcarax@users.noreply.github.com>
2021-04-12 15:26:44 +0200

    arima model

--- Predictive models/Arima_Model.py
+++ Predictive models/Arima_Model.py
@@ -11,7 +11,6 @@ def main():
     series = pd.read_csv('Predictive models\\Data\\MSFT.csv', header=0, index_col=0)
     #test period:
     n = 20
-
     split_point = len(series) - n
     dataset, validation = series[0:split_point], series[split_point:]
     print('Dataset %d, Validation %d' % (len(dataset), len(validation)))
