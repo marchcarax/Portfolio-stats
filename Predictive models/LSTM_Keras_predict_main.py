0e28da08c56a7f9a5d17be300a4f25166079cd3e
Marc <81183798+marchcarax@users.noreply.github.com>
2021-04-14 13:09:16 +0200

    keras lstm main

--- Predictive models/LSTM_Keras_predict_main.py
+++ Predictive models/LSTM_Keras_predict_main.py
@@ -4,8 +4,7 @@ import pandas as pd
 
 def main():
 
-    #Reads csv file and takes the data we need, in this case Adjusted Close prices
-    #and divide in train and test db
+    #Reads csv file to forecast
     series = pd.read_csv('Predictive models\\Data\\MSFT.csv', header=0, index_col=0)
     #test period:
     n = 20
