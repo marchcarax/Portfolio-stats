8a6546f6f6c4a4ef78357af11882c198df278d3d
Marc <81183798+marchcarax@users.noreply.github.com>
2021-04-15 20:48:01 +0200

    keras lstm main

--- Predictive models/LSTM_Keras_predict_main.py
+++ Predictive models/LSTM_Keras_predict_main.py
@@ -1,4 +1,3 @@
-import os
 import LSTM_Keras_model
 import pandas as pd
 
@@ -8,15 +7,13 @@ def main():
     series = pd.read_csv('Predictive models\\Data\\MSFT.csv', header=0, index_col=0)
     #test period:
     n = 20
+    keras_calc(series, n)
 
+
+def keras_calc(series: pd.DataFrame, n: int):
     model = LSTM_Keras_model.Keras_Model(series, n)
     LSTM_Keras_model.Keras_Forecast(model,series, n)
 
 
 if __name__ == '__main__':
-    #due to incompatibilities between tensorflow and numpy, I downgrade numpy
-    try:
-        os.system('pip install numpy==1.19.5')
-    except Exception:
-        pass
     main()
