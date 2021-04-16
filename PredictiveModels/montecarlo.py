732017cc2349c4a670e668e7455e062fad2fbf49
Marc <81183798+marchcarax@users.noreply.github.com>
2021-04-16 16:37:47 +0200

    montecarlo

--- PredictiveModels/montecarlo.py
+++ PredictiveModels/montecarlo.py
@@ -1,12 +1,14 @@
 import pandas as pd
-import numpy as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from tqdm import tqdm
 
-def montecarlo_model(series: pd.DataFrame, n: int):
+def montecarlo_model(series, n):
 
     number_simulation = 100
     predict_day = n
+    close = series['Adj Close'].values
 
     returns = pd.DataFrame(close).pct_change()
     last_price = close[-1]
@@ -21,7 +23,7 @@ def montecarlo_model(series: pd.DataFrame, n: int):
 
     for i in tqdm(range(number_simulation)):
         prices = []
-        prices.append(aux.iloc[-1])
+        prices.append(close[-1])
         for d in range(predict_day):
             shock = [drift + daily_vol * np.random.normal()]
             shock = np.mean(shock)
@@ -41,11 +43,11 @@ def montecarlo_model(series: pd.DataFrame, n: int):
     plt.ylabel('Value')
     plt.xlabel('Simulated days')
     plt.subplot(1,3,2)
-    sns.distplot(aux,norm_hist=True)
-    plt.title('$\mu$ = %.2f, $\sigma$ = %.2f'%(aux.mean(),aux.std()))
+    sns.distplot(close,norm_hist=True)
+    plt.title('$\mu$ = %.2f, $\sigma$ = %.2f'%(close.mean(),close.std()))
     plt.subplot(1,3,3)
     sns.distplot(raveled,norm_hist=True,label='monte carlo samples')
-    sns.distplot(aux,norm_hist=True,label='real samples')
+    sns.distplot(close,norm_hist=True,label='real samples')
     plt.title('simulation $\mu$ = %.2f, $\sigma$ = %.2f'%(raveled.mean(),raveled.std()))
     plt.legend()
     plt.show()
