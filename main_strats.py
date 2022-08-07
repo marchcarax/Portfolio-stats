474e4ecfd387869fea2567b6fca0a19d0f6fbbd1
HERNAM3L <marc.hernandez_fernandez@novartis.com>
2022-08-07 11:54:25 +0200

    updated streamlit dashboard

--- main_strats.py
+++ main_strats.py
@@ -28,8 +28,16 @@ def main():
 
     #Portfolio composition and weight
     #if you have international stocks, remember to put the whole yahoo name with the dot
-    stocks = ['eng.mc','ele.mc', 'itx.mc', 'bbva.mc', 'vid.mc', 'rep.mc', 'csx', 'nvda', 'ibe.mc', 
-             'bac', 'regn', 'msft', 'team', 'fb', 'or.pa', 'atvi', 'gdx', 'spy']
+    portfolio = st.selectbox('Choose portfolio to analyze', ['My portfolio', 'Other'])
+    if portfolio == 'My portfolio':
+        stocks = ['eng.mc','ele.mc', 'itx.mc', 'bbva.mc', 'vid.mc', 'rep.mc', 'csx', 'nvda', 'ibe.mc', 
+                'bac', 'regn', 'msft', 'team', 'fb', 'or.pa', 'atvi', 'gdx']
+    else:
+        st.markdown("Put stock tickets separated by commas without spaces")
+        sl = st.text_input('Stock list:')
+        stocks = sl.split(",")
+
+    stocks.append('spy')
     stocks.sort()
     total_stocks = len(stocks) - 1
     weight = [1/total_stocks]*total_stocks 
@@ -45,7 +53,7 @@ def main():
     returns.rename(columns={'Date':'date', 0:'ret'}, inplace=True)
 
     initial_capital_s1 = st.sidebar.slider('Choose initial capital for strat 1', 10000, 100000, value=50000, step=10000)
-    initial_capital_s2_s3 = st.sidebar.slider('Choose initial capital for strat 2&3', 10000, 100000, value=20000, step=10000)
+    initial_capital_v2 = st.sidebar.slider('Choose initial capital for other strats', 10000, 100000, value=20000, step=10000)
     add_capital = st.sidebar.slider('Choose amount to periodically add', 1000, 5000, value=1000, step=500)
     returns_spy['SPY'] = initial_capital_s1 * returns_spy.SPY
 
@@ -55,13 +63,20 @@ def main():
 
     #Strategy 2: Buy every 3 months
     returns_s2 = returns.copy()
-    returns_s2 = compute_strat_2(returns_s2, initial_capital_s2_s3, add_capital, start_date)
+    returns_s2 = compute_strat_2(returns_s2, initial_capital_v2, add_capital, start_date)
 
     #Strategy 3: Buy everytime RSI dips below 40
     # Define our Lookback period (our sliding window)
     window_length = st.sidebar.slider('Choose window lenght for RSI', 10, 60, value=14, step=1)
     returns_s3 = returns.copy()
-    returns_s3 = compute_strat_3(returns_s3, initial_capital_s2_s3, add_capital, start_date, window_length)
+    returns_s3 = compute_strat_3(returns_s3, initial_capital_v2, add_capital, start_date, window_length)
+
+    #Strategy 4: Buy everytime rolling sharpe cycles lower
+    # Define our Lookback period (our sliding window)
+    buy_signal = st.sidebar.slider('Choose window lenght for RSI', -0.5, 0.0, value=-0.1, step=0.1)
+    returns_s4 = returns.copy()
+    returns_s4["sharpe"] = rolling_sharpe(returns_s4.ret)
+    returns_s4 = compute_strat_4(returns_s4, initial_capital_v2, add_capital, start_date, buy_signal)
 
     #st.dataframe(returns_s3)
 
@@ -70,6 +85,7 @@ def main():
     df_total['benchmark'] = returns_spy.SPY
     df_total['ret_s2'] = returns_s2.ret
     df_total['ret_s3'] = returns_s3.ret
+    df_total['ret_s4'] = returns_s4.ret
     fig = prepare_full_graph(df_total)
     st.plotly_chart(fig, use_container_width=False)
 
@@ -103,8 +119,25 @@ def main():
     fig = px.line(returns_s3, x="date", y='buy')
     st.plotly_chart(fig, use_container_width=False)
 
+    st.markdown('#### Strategy 4: Buy everytime rolling sharpe cycles lower')
+    st.markdown('After an initial capital investment, we add capital every month when rolling Sharpe ratio cycles lower than 0')
+    mean, stdev = portfolio_info(returns_s4.drop(['sharpe', 'buy'], axis=1))
+    st.write('Portfolio expected annualised return is {} and volatility is {}'.format(mean, stdev))
+    st.write('Portfolio sharpe ratio is {}'.format((mean - risk_free_return)/stdev))
+
+    st.markdown('##### Rolling sharpe graph')
+    fig = px.line(returns_s4, x="date", y='sharpe')
+    fig.add_hline(y=buy_signal, line_color="green", line_dash="dash")
+    fig.add_hline(y=0.5, line_color="red", line_dash="dash")
+    st.plotly_chart(fig, use_container_width=False)
+    st.write('Last rolling sharpe data point is {}'.format(returns_s4.sharpe[-1:].values))
+
+    st.markdown('##### Buy signals for Strat 4')
+    fig = px.line(returns_s4, x="date", y='buy')
+    st.plotly_chart(fig, use_container_width=False)
+
 def prepare_full_graph(df):
-    return px.line(df, x="date", y=['benchmark', 'ret', 'ret_s2', 'ret_s3'])
+    return px.line(df, x="date", y=['benchmark', 'ret', 'ret_s2', 'ret_s3', 'ret_s4'])
 
 def portfolio_info(stocks):
 
@@ -116,6 +149,9 @@ def portfolio_info(stocks):
     portfolio_std_dev = round(np.sqrt(cov_data) * np.sqrt(252),2)
 
     return portfolio_return*100, portfolio_std_dev*100
+
+def rolling_sharpe(df, n = 20, risk_free = 0):
+    return (df.pct_change().rolling(n).mean() - risk_free) / df.pct_change().rolling(n).std()
 
 def compute_strat_2(df, capital, add_capital, start_date):
 
@@ -163,6 +199,24 @@ def compute_strat_3(df, capital, add_capital, start_date, n):
     add = False
     for idx, row in df.iterrows():
         if (row.rsi < 35) and (row.date > date_to_add):
+            capital += add_capital
+            date_to_add = row['date'] + datetime.timedelta(days=30)
+            add  = True
+        df.at[idx,'ret'] *= capital
+        if add: df.at[idx,'buy'] = 1
+        add = False
+
+    return df
+
+
+def compute_strat_4(df, capital, add_capital, start_date, buy):
+
+    df['buy'] = 0
+
+    date_to_add = start_date + datetime.timedelta(days=30)
+    add = False
+    for idx, row in df.iterrows():
+        if (row.sharpe < buy) and (row.date > date_to_add):
             capital += add_capital
             date_to_add = row['date'] + datetime.timedelta(days=30)
             add  = True
