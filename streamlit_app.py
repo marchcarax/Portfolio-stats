dc34bad2aa55d00de8650f4019dce3f0ec14f78a
HERNAM3L <marc.hernandez_fernandez@novartis.com>
2022-10-31 10:08:37 +0100

    stock change

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ def main():
     portfolio = st.selectbox('Choose portfolio to analyze', ['My portfolio', 'Other'])
     if portfolio == 'My portfolio':
         stocks = ['eng.mc','ele.mc', 'itx.mc', 'bbva.mc', 'vid.mc', 'rep.mc', 'csx', 'nvda', 'ibe.mc', 
-                'azn', 'regn', 'msft', 'team', 'meta', 'or.pa', 'atvi', 'san.pa', 'tsla']
+                'azn', 'regn', 'msft', 'team', 'or.pa', 'atvi', 'san.pa', 'tsla']
     else:
         st.markdown("Put stock tickets separated by commas without spaces (e.g. qqq,msft,aapl,ibe.mc)")
         sl = st.text_input('Stock list:')
